cf2a89cf684a163db3d7aa62178a9935983da6f9
DhemesonSiqueira <dhemeson_loran@hotmail.com>
2021-05-19 20:54:56 -0300

    Initial commit

--- classes/Usuario.class.php
+++ classes/Usuario.class.php
@@ -1,19 +1,32 @@
 <?php
-    session_start();
+    class Usuario{
+        protected $id;
+        protected $nome;
+        protected $usuario;
+        protected $email;
+        protected $senha;
 
-    if(isset($_POST['botao']) && $_POST['botao'] == "Logar"){
-        include("classes/DB.class.php");
-        include("classes/Usuario.class.php");
-    
-        $login = Usuario::logar($_POST['login'], $_POST['senha']);
-    
-        if(isset($_SESSION['login']) && isset($_SESSION['id'])){
-            echo "Você está Logado <a href='index.php'> ACESSE O SISTEMA</a>";
-            
-        }else{
-            echo "Você NÃO está logado";
+        public static function logar($usuario, $senha){
+            if($usuario && $senha){
+                $sql = "SELECT * FROM usuario WHERE usuario = :usuario AND senha = :senha";
+                $stmt = DB::conexao()->prepare($sql);
+                $stmt->bindParam(':usuario', $usuario);
+                $stmt->bindParam(':senha',  $senha);
+                $stmt->execute();
+
+                foreach($stmt as $obj){
+                    $_SESSION['login'] = $obj['usuario'];
+                    $_SESSION['id'] = $obj['id_usuario'];
+                }
+
+            }
+
         }
+
+
+
     }
+
 
 ?>
 
